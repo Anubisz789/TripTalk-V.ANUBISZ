7e64a69eab49860a03fa0641289b2c9f9079f0fb
Anubisz789 <masterkungth0011@gmail.com>
2026-04-18 16:19:35 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,4 @@
-const CACHE_NAME = 'triptalk-v4.1.0';
-
-// [Architect Fix] ใช้ relative paths เพื่อให้ cache ทำงานได้ไม่ว่าจะ deploy ที่ root หรือ sub-path
+const CACHE_NAME = 'triptalk-v4.3-pwa-fix';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
@@ -14,34 +12,35 @@ const ASSETS_TO_CACHE = [
 ];
 
 self.addEventListener('install', (event) => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             console.log('[SW] Pre-caching assets');
             return cache.addAll(ASSETS_TO_CACHE);
         })
     );
-    self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
     event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cache) => {
-                    if (cache !== CACHE_NAME) {
-                        console.log('[SW] Clearing old cache:', cache);
-                        return caches.delete(cache);
-                    }
-                })
-            );
-        })
+        Promise.all([
+            self.clients.claim(),
+            caches.keys().then((cacheNames) => {
+                return Promise.all(
+                    cacheNames.map((cache) => {
+                        if (cache !== CACHE_NAME) {
+                            console.log('[SW] Clearing old cache:', cache);
+                            return caches.delete(cache);
+                        }
+                    })
+                );
+            })
+        ])
     );
-    self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
     if (!event.request.url.startsWith('http')) return;
-
     event.respondWith(
         fetch(event.request).catch(() => {
             return caches.match(event.request);
